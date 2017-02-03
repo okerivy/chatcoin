6a627405f4335fb1bc36789cb8533f22152a61f1
EricLee <okerivy@gmail.com>
2017-02-03 20:38:45 +0800

    初始化项目

---
+++ chatcoin/chatcoin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chatcoin
+//
+//  Created by okerivy on 2017/2/3.
+//  Copyright © 2017年 okerivy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chatcoin/chatcoin/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  chatcoin
+//
+//  Created by okerivy on 2017/2/3.
+//  Copyright © 2017年 okerivy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
