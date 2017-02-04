0eeb06460978b6ba4434cce3f9410fbfb9942ce0
okerivy <okerivy@163.com>
2017-02-04 22:11:29 +0800

    目录结构

---
+++ chatcoin/chatcoin/Class/Tools/Config/Api.h
@@ -0,0 +1,13 @@
+//
+//  Api.h
+//  chatcoin
+//
+//  Created by okerivy on 2017/2/4.
+//  Copyright © 2017年 okerivy. All rights reserved.
+//
+
+#ifndef Api_h
+#define Api_h
+
+
+#endif /* Api_h */
---
+++ chatcoin/chatcoin/Class/Tools/Config/Config.h
@@ -0,0 +1,13 @@
+//
+//  Config.h
+//  chatcoin
+//
+//  Created by okerivy on 2017/2/4.
+//  Copyright © 2017年 okerivy. All rights reserved.
+//
+
+#ifndef Config_h
+#define Config_h
+
+
+#endif /* Config_h */
